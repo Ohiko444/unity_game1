cf70cdf713f356c968d03ec340073b81846da8f1
Ohiko444 <125536430+Ohiko444@users.noreply.github.com>
2024-04-05 12:23:07 +0300

    3 commit
    реализована кнопка закрытия rate

--- My project 1/Assets/Scripts/RateComponent.cs
+++ My project 1/Assets/Scripts/RateComponent.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RateComponent : MonoBehaviour
 {
 
     public CanvasGroup canvasGroup;
 
+    public Button CloseRate;
+
+    private void Start()
+    {
+        CloseRate.onClick.AddListener(Close);
+    }
+
     public void Open()
     {
         gameObject.SetActive(true);
         canvasGroup.alpha = 0.0f;
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
     }
 
     void Update()
